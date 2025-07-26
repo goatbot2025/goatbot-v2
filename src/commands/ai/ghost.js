c81aa6ab9bdadf6e67f2aea8ae38b72145fe0331
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-26 20:55:21 +0600

    Update ghost.js

--- src/commands/ai/ghost.js
+++ src/commands/ai/ghost.js
@@ -1,1 +1,23 @@
+module.exports = {
+  config: {
+    name: "ghost",
+    aliases: [],
+    version: "1.0",
+    author: "Newton",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Ghost text",
+    longDescription: "Send ghost (invisible) message",
+    category: "ai",
+    guide: {
+      en: "{pn} [message]"
+    }
+  },
 
+  onStart: async function ({ api, event, args }) {
+    const msg = args.join(" ");
+    if (!msg) return api.sendMessage("👻 Please enter a message to ghost.", event.threadID);
+    const ghost = msg.split("").join("\u200B");
+    api.sendMessage(ghost, event.threadID);
+  }
+};
