1643d98f13314365315d5208f05d01db98459463
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-26 21:03:06 +0600

    Update ban.js

--- src/commands/boxchat/ban.js
+++ src/commands/boxchat/ban.js
@@ -1,1 +1,23 @@
+module.exports = {
+  config: {
+    name: "ban",
+    version: "1.0",
+    author: "Newton",
+    role: 2,
+    shortDescription: "⛔ Ban user",
+    longDescription: "Ban a user from using the bot",
+    category: "boxchat",
+    guide: "{pn} [@mention]"
+  },
+
+  onStart: async function ({ api, event }) {
+    const mention = Object.keys(event.mentions)[0];
+    if (!mention) return api.sendMessage("🛑 Mention someone to ban.", event.threadID);
+
+    global.bannedUsers = global.bannedUsers || new Set();
+    global.bannedUsers.add(mention);
+
+    return api.sendMessage(`⛔ User ${mention} has been banned.`, event.threadID);
+  }
+};
 
