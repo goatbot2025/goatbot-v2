fcf68cd5f5bfb880ec16f8fc4d46a33ceeade60e
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 12:27:21 +0600

    Update sendnoti.js

--- src/commands/boxchat/sendnoti.js
+++ src/commands/boxchat/sendnoti.js
@@ -1,1 +1,24 @@
+module.exports = {
+  config: {
+    name: "sendnoti",
+    aliases: [],
+    version: "1.0",
+    author: "Newton",
+    countDown: 3,
+    role: 1,
+    shortDescription: "Send notification",
+    longDescription: "Send notification to all threads",
+    category: "boxchat",
+    guide: { en: "{pn} message" }
+  },
 
+  onStart: async function ({ api, args }) {
+    const msg = args.join(" ");
+    const threads = global.data.allThreadID || [];
+
+    for (const tid of threads) {
+      api.sendMessage(msg, tid);
+      await new Promise(r => setTimeout(r, 500));
+    }
+  }
+};
