17ee82a183b31b0459ecc81c277ae721f433f398
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:25:07 +0600

    Update supportgc.js

--- src/commands/group/supportgc.js
+++ src/commands/group/supportgc.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  config: {
+    name: "supportgc",
+    version: "1.0",
+    author: "Newton",
+    description: "Get the support group link",
+    usage: "",
+    cooldown: 3
+  },
+  run: async function({ message }) {
+    return message.reply("📌 Join our support group: https://facebook.com/groups/yourgroupid");
+  }
+};
