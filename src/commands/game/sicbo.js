21aee35d65b85536a8e0b7808ac8af519a5c1006
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:16:54 +0600

    Update sicbo.js

--- src/commands/game/sicbo.js
+++ src/commands/game/sicbo.js
@@ -1,1 +1,14 @@
-
+module.exports = {
+  config: {
+    name: "sicbo",
+    version: "1.0",
+    author: "Newton",
+    description: "Dice roll game",
+    usage: "",
+    cooldown: 5,
+  },
+  run: async function ({ message }) {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    message.reply(`🎲 You rolled a ${roll}`);
+  },
+};
