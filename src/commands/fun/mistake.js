81c2c9d317e33077d9f4d213626698b99b0d3d07
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:05:14 +0600

    Update mistake.js

--- src/commands/fun/mistake.js
+++ src/commands/fun/mistake.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  config: {
+    name: "mistake",
+    version: "1.0",
+    author: "Newton",
+    description: "Admit a mistake in a funny way",
+    usage: "",
+    cooldown: 5,
+  },
+  run: async function ({ message }) {
+    message.reply("I admit... my biggest mistake was trusting you with my snacks. 🍪😭");
+  },
+};
