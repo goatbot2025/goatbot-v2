a87f3a44b88908081a2eac9540d422680ab36cb2
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:06:30 +0600

    Update rip.js

--- src/commands/fun/rip.js
+++ src/commands/fun/rip.js
@@ -1,1 +1,16 @@
-
+module.exports = {
+  config: {
+    name: "rip",
+    version: "1.0",
+    author: "Newton",
+    description: "Rest in peace meme",
+    usage: "[mention/reply]",
+    cooldown: 5,
+  },
+  run: async function ({ message, event, Users }) {
+    const uid = event.messageReply?.senderID || Object.keys(event.mentions)[0];
+    if (!uid) return message.reply("Tag someone to RIP 👻");
+    const name = await Users.getName(uid);
+    message.reply(`🪦 R.I.P ${name}.\nGone but never forgotten. 😔`);
+  },
+};
