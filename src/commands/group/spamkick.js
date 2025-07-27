59efe5fe7552311afbdf3522481632a5d09a7f38
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:24:45 +0600

    Update spamkick.js

--- src/commands/group/spamkick.js
+++ src/commands/group/spamkick.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  config: {
+    name: "spamkick",
+    version: "1.0",
+    author: "Newton",
+    description: "Kick users sending repeated messages",
+    usage: "",
+    cooldown: 5
+  },
+  run: async function({ message, event, api }) {
+    return message.reply("🚫 Spam kick functionality is not implemented yet.");
+  }
+};
