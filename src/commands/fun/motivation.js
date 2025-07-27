132f70a4e1d4857087ebf0f689d1f9f051ee2ef0
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:05:54 +0600

    Update motivation.js

--- src/commands/fun/motivation.js
+++ src/commands/fun/motivation.js
@@ -1,1 +1,19 @@
-
+module.exports = {
+  config: {
+    name: "motivation",
+    version: "1.0",
+    author: "Newton",
+    description: "Send a motivational quote",
+    usage: "",
+    cooldown: 5,
+  },
+  run: async function ({ message }) {
+    const quotes = [
+      "Believe in yourself and all that you are. 💪",
+      "Push yourself, because no one else is going to do it for you. 🚀",
+      "Don’t watch the clock; do what it does. Keep going. ⏰",
+    ];
+    const random = quotes[Math.floor(Math.random() * quotes.length)];
+    message.reply(random);
+  },
+};
