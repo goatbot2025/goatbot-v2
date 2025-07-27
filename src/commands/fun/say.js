745fcabe831eb7d8af0a3dacb99bb5574a1eb6df
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:07:05 +0600

    Update say.js

--- src/commands/fun/say.js
+++ src/commands/fun/say.js
@@ -1,1 +1,14 @@
-
+module.exports = {
+  config: {
+    name: "say",
+    version: "1.0",
+    author: "Newton",
+    description: "Repeat your message",
+    usage: "[text]",
+    cooldown: 3,
+  },
+  run: async function ({ message, args }) {
+    if (!args[0]) return message.reply("Say what? 🤔");
+    message.reply(args.join(" "));
+  },
+};
