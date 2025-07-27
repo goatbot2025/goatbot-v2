eb690945b3b0f226dc18662662b3724714eac615
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:08:05 +0600

    Update toilet.js

--- src/commands/fun/toilet.js
+++ src/commands/fun/toilet.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  config: {
+    name: "toilet",
+    version: "1.0",
+    author: "Newton",
+    description: "Send a toilet meme",
+    usage: "",
+    cooldown: 5,
+  },
+  run: async function ({ message }) {
+    message.reply("🚽 Toilet break! Don’t forget the tissue. 🧻😂");
+  },
+};
