d2fba8d7019881d7116f50dd3dbc4f9146327f11
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:15:05 +0600

    Update guessnumber.js

--- src/commands/game/guessnumber.js
+++ src/commands/game/guessnumber.js
@@ -1,1 +1,14 @@
-
+module.exports = {
+  config: {
+    name: "guessnumber",
+    version: "1.0",
+    author: "Newton",
+    description: "Guess a number between 1 and 10",
+    usage: "",
+    cooldown: 5,
+  },
+  run: async function ({ message }) {
+    const num = Math.floor(Math.random() * 10) + 1;
+    message.reply(`🎲 I guessed: ${num}. Can you guess better?`);
+  },
+};
