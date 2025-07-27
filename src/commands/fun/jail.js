bfe87cb56b690e452fa2db6468789cf4d18fcbf6
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:03:05 +0600

    Update jail.js

--- src/commands/fun/jail.js
+++ src/commands/fun/jail.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  config: {
+    name: "jail",
+    version: "1.0",
+    author: "Newton",
+    description: "Send a jail image",
+    usage: "[mention/reply]",
+    cooldown: 5,
+  },
+  run: async function ({ message, event }) {
+    message.reply("🔒 You're under arrest and going straight to jail! 🚔");
+  },
+};
