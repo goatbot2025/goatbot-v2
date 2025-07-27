ff69e93229ea0747a4df1f45e094b848b1bcc3be
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:02:15 +0600

    Update friend.js

--- src/commands/fun/friend.js
+++ src/commands/fun/friend.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  config: {
+    name: "friend",
+    version: "1.0",
+    author: "Newton",
+    description: "Send a friendship message",
+    usage: "",
+    cooldown: 5,
+  },
+  run: async function ({ message }) {
+    message.reply("Friendship is the only cement that will ever hold the world together 🤝💖");
+  },
+};
