10f2cd8255d4bbc32dbd21a97e20e03e04d28edf
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:23:23 +0600

    Update expend.js

--- src/commands/gen/expend.js
+++ src/commands/gen/expend.js
@@ -1,1 +1,15 @@
-
+module.exports = {
+  config: {
+    name: "expend",
+    version: "1.0",
+    author: "Newton",
+    description: "Expand text with spaces",
+    usage: "[text]",
+    cooldown: 3
+  },
+  run: async function({ message, args }) {
+    if (!args[0]) return message.reply("❗ Provide some text.");
+    const expanded = args.join(" ").split("").join(" ");
+    message.reply(`🔠 ${expanded}`);
+  }
+};
