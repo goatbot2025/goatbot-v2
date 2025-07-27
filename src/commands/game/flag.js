727378c8347ebfbc2dac3500f6834bba8882a55a
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:14:34 +0600

    Update flag.js

--- src/commands/game/flag.js
+++ src/commands/game/flag.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  config: {
+    name: "flag",
+    version: "1.0",
+    author: "Newton",
+    description: "Guess the country from flag (coming soon)",
+    usage: "",
+    cooldown: 5,
+  },
+  run: async function ({ message }) {
+    message.reply("🚩 Flag guessing game is coming soon!");
+  },
+};
