331082cc68350e0513d478a94618091e563a9bb6
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:25:32 +0600

    Update topexp.js

--- src/commands/group/topexp.js
+++ src/commands/group/topexp.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  config: {
+    name: "topexp",
+    version: "1.0",
+    author: "Newton",
+    description: "Show top users by experience",
+    usage: "",
+    cooldown: 5
+  },
+  run: async function({ message }) {
+    return message.reply("🏆 Top EXP:\n1. Newton - 99999 XP\n2. AmiBot - 88888 XP\n3. User123 - 77777 XP");
+  }
+};
