d6071921b60f56a3aa48d0179bb1077321e92dbc
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 12:25:43 +0600

    Update refresh.js

--- src/commands/boxchat/refresh.js
+++ src/commands/boxchat/refresh.js
@@ -1,1 +1,19 @@
+module.exports = {
+  config: {
+    name: "refresh",
+    aliases: [],
+    version: "1.0",
+    author: "Newton",
+    countDown: 3,
+    role: 1,
+    shortDescription: "Refresh bot",
+    longDescription: "Restart or reload the bot's memory",
+    category: "boxchat",
+    guide: { en: "{pn}" }
+  },
 
+  onStart: async function ({ message }) {
+    message.reply("🔄 Bot is refreshing...");
+    process.exit(1);
+  }
+};
