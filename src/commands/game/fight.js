a3b49b92d5cfbcf12aa35af615ea7610f93c5289
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-27 13:14:00 +0600

    Update fight.js

--- src/commands/game/fight.js
+++ src/commands/game/fight.js
@@ -1,1 +1,15 @@
-
+module.exports = {
+  config: {
+    name: "fight",
+    version: "1.0",
+    author: "Newton",
+    description: "Fight with another user",
+    usage: "[tag/reply]",
+    cooldown: 5,
+  },
+  run: async function ({ message, event }) {
+    const uid = event.messageReply?.senderID || Object.keys(event.mentions)[0];
+    if (!uid) return message.reply("Tag someone to fight! 🥊");
+    message.reply("🥊 Fight started! (Coming soon)");
+  },
+};
