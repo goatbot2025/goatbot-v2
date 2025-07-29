7f81bcccb49676250975c937a1493e8fed29646a
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-29 20:53:17 +0600

    Update slap.js

--- src/commands/fun/slap.js
+++ src/commands/fun/slap.js
@@ -1,1 +1,23 @@
-
+module.exports = {
+  config: {
+    name: "slap",
+    version: "1.0",
+    author: "newton",
+    countDown: 4,
+    role: 0,
+    shortDescription: "Slap someone",
+    longDescription: "Slap a user with anime slap gif",
+    category: "fun",
+    guide: "{pn} @mention"
+  },
+  onStart: async function ({ event, message }) {
+    const mention = Object.keys(event.mentions)[0];
+    if (!mention) return message.reply("Tag someone to slap 👋");
+    const res = await global.utils.getStreamFromURL("https://nekos.life/api/v2/img/slap");
+    const data = await res.json();
+    return message.send({
+      body: `👋 ${event.senderID} slapped ${mention}!`,
+      attachment: await global.utils.getStreamFromURL(data.url)
+    });
+  }
+};
