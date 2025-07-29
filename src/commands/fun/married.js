cfb0b60edbef985d19caeb124a7d5e0bb83bdc51
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-29 14:23:06 +0600

    Update married.js

--- src/commands/fun/married.js
+++ src/commands/fun/married.js
@@ -1,16 +1,1 @@
-module.exports = {
-  config: {
-    name: "married",
-    version: "1.0",
-    author: "Newton",
-    description: "Fake marriage proposal",
-    usage: "[mention/reply]",
-    cooldown: 5,
-  },
-  run: async function ({ message, event, Users }) {
-    const uid = event.messageReply?.senderID || Object.keys(event.mentions)[0];
-    if (!uid) return message.reply("Mention someone to propose 💍");
-    const name = await Users.getName(uid);
-    message.reply(`💍 You are now officially married to ${name}! 🎉`);
-  },
-};
+
