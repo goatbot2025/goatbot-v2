8a85c4f2c8d3fb813f04dbfbe5b6f21de3ad6ee0
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-29 23:15:15 +0600

    Update wanted.js

--- src/commands/fun/wanted.js
+++ src/commands/fun/wanted.js
@@ -1,1 +1,21 @@
-
+module.exports = {
+  config: {
+    name: "wanted",
+    version: "1.0",
+    author: "newton",
+    countDown: 3,
+    role: 0,
+    shortDescription: "Wanted poster",
+    longDescription: "Creates a wanted poster of user",
+    category: "fun",
+    guide: "{pn} @mention"
+  },
+  onStart: async function ({ event, message }) {
+    const mention = Object.keys(event.mentions)[0] || event.senderID;
+    const url = `https://some-random-api.com/canvas/wanted?avatar=https://graph.facebook.com/${mention}/picture?width=512&height=512`;
+    message.send({
+      body: `🔫 WANTED!`,
+      attachment: await global.utils.getStreamFromURL(url)
+    });
+  }
+};
