13ed98aa3cdc7d05b9d825d9825520f18bec854a
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-29 14:21:45 +0600

    Update hack.js

--- src/commands/fun/hack.js
+++ src/commands/fun/hack.js
@@ -1,15 +1,1 @@
-module.exports = {
-  config: {
-    name: "hack",
-    version: "1.0",
-    author: "Newton",
-    description: "Fake hack someone",
-    usage: "[mention/reply]",
-    cooldown: 5,
-  },
-  run: async function ({ message, event, Users }) {
-    const targetID = event.messageReply?.senderID || event.mentions[0] || event.senderID;
-    const name = await Users.getName(targetID);
-    message.reply(`Hacking ${name}...\n🔍 Finding IP...\n💻 Injecting virus...\n✅ Done! 😂`);
-  },
-};
+
