6809287b2512526dab51643f36a6d7033c46a842
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-29 14:27:39 +0600

    Update trigger.js

--- src/commands/fun/trigger.js
+++ src/commands/fun/trigger.js
@@ -1,15 +1,1 @@
-module.exports = {
-  config: {
-    name: "trigger",
-    version: "1.0",
-    author: "Newton",
-    description: "Send a triggered meme (placeholder)",
-    usage: "[mention/reply]",
-    cooldown: 5,
-  },
-  run: async function ({ message, event }) {
-    const uid = event.messageReply?.senderID || Object.keys(event.mentions)[0];
-    if (!uid) return message.reply("Tag someone to TRIGGER! 😤");
-    message.reply("🔴 Triggered! 😡 (Feature to be added)");
-  },
-};
+
