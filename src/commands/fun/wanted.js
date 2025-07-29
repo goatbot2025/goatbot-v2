4d7a46e1f21f42b2082bf8ebd669c77761a8c172
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-29 14:28:06 +0600

    Update wanted.js

--- src/commands/fun/wanted.js
+++ src/commands/fun/wanted.js
@@ -1,15 +1,1 @@
-module.exports = {
-  config: {
-    name: "wanted",
-    version: "1.0",
-    author: "Newton",
-    description: "Generate a wanted poster (placeholder)",
-    usage: "[mention/reply]",
-    cooldown: 5,
-  },
-  run: async function ({ message, event }) {
-    const uid = event.messageReply?.senderID || Object.keys(event.mentions)[0];
-    if (!uid) return message.reply("Tag someone to WANTED! 🧾");
-    message.reply("📜 Wanted Poster Coming Soon!");
-  },
-};
+
