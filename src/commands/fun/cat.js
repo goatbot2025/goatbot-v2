f9a8b7cc92212dbd65276ab0b9eb3f6063fc9dba
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-29 20:50:10 +0600

    Update cat.js

--- src/commands/fun/cat.js
+++ src/commands/fun/cat.js
@@ -1,1 +1,22 @@
-
+module.exports = {
+  config: {
+    name: "cat",
+    version: "1.0",
+    author: "newton",
+    countDown: 3,
+    role: 0,
+    shortDescription: "Random cat image",
+    longDescription: "Send a cute random cat image",
+    category: "fun",
+    guide: "{pn}"
+  },
+  onStart: async function ({ message }) {
+    const res = await global.utils.getStreamFromURL("https://api.thecatapi.com/v1/images/search");
+    const data = await res.json();
+    const imgURL = data[0].url;
+    return message.send({
+      body: "🐱 Here's a cute cat for you!",
+      attachment: await global.utils.getStreamFromURL(imgURL)
+    });
+  }
+};
