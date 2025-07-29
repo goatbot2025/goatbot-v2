7a95d26c9d146056263c76fb38d08bc3264b4117
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-29 23:11:06 +0600

    Update faceplam.js

--- src/commands/fun/faceplam.js
+++ src/commands/fun/faceplam.js
@@ -1,1 +1,20 @@
-
+module.exports = {
+  config: {
+    name: "faceplam",
+    version: "1.0",
+    author: "newton",
+    countDown: 2,
+    role: 0,
+    shortDescription: "Facepalm gif",
+    longDescription: "Send a facepalm image",
+    category: "fun",
+    guide: "{pn}"
+  },
+  onStart: async function ({ message }) {
+    const res = await global.utils.getStreamFromURL("https://nekos.life/api/v2/img/facepalm");
+    message.send({
+      body: "🤦‍♂️ Facepalm...",
+      attachment: res
+    });
+  }
+};
