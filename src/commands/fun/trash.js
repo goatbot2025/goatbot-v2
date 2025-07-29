788ce74cc2466508e1140a9c23e9d15ff068221f
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-29 14:27:09 +0600

    Update trash.js

--- src/commands/fun/trash.js
+++ src/commands/fun/trash.js
@@ -1,16 +1,1 @@
-module.exports = {
-  config: {
-    name: "trash",
-    version: "1.0",
-    author: "Newton",
-    description: "Send trash message",
-    usage: "[mention/reply]",
-    cooldown: 3,
-  },
-  run: async function ({ message, event, Users }) {
-    const uid = event.messageReply?.senderID || Object.keys(event.mentions)[0];
-    if (!uid) return message.reply("Mention someone to trash 🗑️");
-    const name = await Users.getName(uid);
-    message.reply(`🗑️ ${name} has been thrown into the trash can! 😂`);
-  },
-};
+
