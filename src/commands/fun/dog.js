8a0babd4ea9fffbbf8fddac1149758414206a26d
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-29 20:50:53 +0600

    Update dog.js

--- src/commands/fun/dog.js
+++ src/commands/fun/dog.js
@@ -1,1 +1,22 @@
-
+module.exports = {
+  config: {
+    name: "dog",
+    version: "1.0",
+    author: "newton",
+    countDown: 3,
+    role: 0,
+    shortDescription: "Random dog image",
+    longDescription: "Send a random dog image",
+    category: "fun",
+    guide: "{pn}"
+  },
+  onStart: async function ({ message }) {
+    const res = await global.utils.getStreamFromURL("https://dog.ceo/api/breeds/image/random");
+    const data = await res.json();
+    const imgURL = data.message;
+    return message.send({
+      body: "🐶 Here's a cute dog for you!",
+      attachment: await global.utils.getStreamFromURL(imgURL)
+    });
+  }
+};
