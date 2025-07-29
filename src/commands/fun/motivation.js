8fba672d84425788f8cf3e7944c148e5e0abf22e
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-29 23:12:56 +0600

    Update motivation.js

--- src/commands/fun/motivation.js
+++ src/commands/fun/motivation.js
@@ -1,1 +1,20 @@
-
+module.exports = {
+  config: {
+    name: "motivation",
+    version: "1.0",
+    author: "newton",
+    countDown: 3,
+    role: 0,
+    shortDescription: "Motivational quote",
+    longDescription: "Send a motivational image",
+    category: "fun",
+    guide: "{pn}"
+  },
+  onStart: async function ({ message }) {
+    const url = "https://some-random-api.com/canvas/misc/motivation";
+    message.send({
+      body: "💪 Stay motivated!",
+      attachment: await global.utils.getStreamFromURL(url)
+    });
+  }
+};
