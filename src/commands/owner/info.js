4db0344a523e03d6ba4053a110f3869ee2675bb4
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-30 19:20:33 +0600

    Update info.js

--- src/commands/owner/info.js
+++ src/commands/owner/info.js
@@ -1,1 +1,23 @@
+module.exports = {
+  config: {
+    name: "info",
+    version: "1.0",
+    author: "newton",
+    role: 2,
+    shortDescription: "Bot information",
+    longDescription: "Displays basic bot and owner information",
+    category: "owner",
+    guide: "{pn}"
+  },
+  onStart: async function ({ message }) {
+    message.reply(`🤖 Bot Info:
+• Owner: Newton
+• UID: 100095322679638
+• Bot Nick: music-lover (newton)
+• Bot UID: 61577598163835
+• Prefix: *
 
+🔗 Owner FB: https://www.facebook.com/newton.biswas.504758
+🔗 Bot FB: https://www.facebook.com/profile.php?id=61577598163835`);
+  }
+};
