59a30c204e18bf921635a1266d35f87efd7fc0e3
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-30 19:00:23 +0600

    Update numinfo.js

--- src/commands/info/numinfo.js
+++ src/commands/info/numinfo.js
@@ -1,1 +1,27 @@
+const axios = require("axios");
 
+module.exports = {
+  config: {
+    name: "numinfo",
+    version: "1.0",
+    author: "newton",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Number information",
+    longDescription: "Get information about a number",
+    category: "info",
+    guide: "{pn} <number>"
+  },
+
+  onStart: async function ({ message, args }) {
+    const number = args[0];
+    if (!number) return message.send("🔢 কোনো সংখ্যা দাও!");
+
+    try {
+      const res = await axios.get(`http://numbersapi.com/${number}`);
+      message.send(`ℹ️ ${res.data}`);
+    } catch {
+      message.send("❌ তথ্য খুঁজে পাওয়া যায়নি।");
+    }
+  }
+};
