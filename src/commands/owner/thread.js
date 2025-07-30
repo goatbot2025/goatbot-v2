68087af1396e93aa6a863ccccc1ac7ec090a31cc
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-30 19:14:10 +0600

    Update thread.js

--- src/commands/owner/thread.js
+++ src/commands/owner/thread.js
@@ -1,1 +1,22 @@
+module.exports = {
+  config: {
+    name: "thread",
+    version: "1.0",
+    author: "newton",
+    role: 2,
+    shortDescription: "Get thread info",
+    longDescription: "Displays information about the current group thread",
+    category: "owner",
+    guide: "{pn}"
+  },
 
+  onStart: async function ({ threadsData, event, message }) {
+    const thread = await threadsData.get(event.threadID);
+    message.reply(`🧵 Thread Info:
+• Name: ${thread.threadName}
+• ID: ${thread.threadID}
+• Emoji: ${thread.emoji}
+• Admins: ${thread.adminIDs.length}
+• Member count: ${thread.members.length}`);
+  }
+};
