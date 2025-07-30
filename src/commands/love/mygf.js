6f641bf709b204308a5926ba0785792b590c9762
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-30 18:45:24 +0600

    Update mygf.js

--- src/commands/love/mygf.js
+++ src/commands/love/mygf.js
@@ -1,1 +1,19 @@
+module.exports = {
+  config: {
+    name: "mygf",
+    version: "1.0",
+    author: "newton",
+    countDown: 1,
+    role: 0,
+    shortDescription: "your GF",
+    longDescription: "Shows your imaginary girlfriend",
+    category: "love",
+    guide: "{pn}"
+  },
 
+  onStart: async function ({ message }) {
+    const names = ["Anika", "Sweety", "Riya", "Mahi", "Tina", "Tumpa"];
+    const pick = names[Math.floor(Math.random() * names.length)];
+    message.send(`🌸 তোমার গার্লফ্রেন্ড: ${pick}`);
+  }
+};
