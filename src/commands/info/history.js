d9dea5cd311a5ec77053bd11dcd1dacc2af902e9
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-30 18:59:23 +0600

    Update history.js

--- src/commands/info/history.js
+++ src/commands/info/history.js
@@ -1,1 +1,25 @@
+const axios = require("axios");
 
+module.exports = {
+  config: {
+    name: "history",
+    version: "1.0",
+    author: "newton",
+    countDown: 10,
+    role: 0,
+    shortDescription: "Today in history",
+    longDescription: "Get historical facts for today",
+    category: "info",
+    guide: "{pn}"
+  },
+
+  onStart: async function ({ message }) {
+    try {
+      const res = await axios.get("http://history.muffinlabs.com/date");
+      const data = res.data.data.Events[0];
+      message.send(`📅 ${data.year}: ${data.text}`);
+    } catch (err) {
+      message.send("❌ ইতিহাস আনতে সমস্যা হয়েছে।");
+    }
+  }
+};
