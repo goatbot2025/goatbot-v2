67fbd5e45254f3cf43c21c2fdcd20306ed176ebf
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-30 19:21:13 +0600

    Update ignoreonlyadbox.js

--- src/commands/owner/ignoreonlyadbox.js
+++ src/commands/owner/ignoreonlyadbox.js
@@ -1,1 +1,15 @@
-
+module.exports = {
+  config: {
+    name: "ignoreonlyadbox",
+    version: "1.0",
+    author: "newton",
+    role: 2,
+    shortDescription: "Ignore admin in current box",
+    longDescription: "This command will ignore admin in current group only",
+    category: "owner",
+    guide: "{pn}"
+  },
+  onStart: async function ({ event, message }) {
+    message.reply(`✅ Admin-only ignore is on for this thread (${event.threadID})`);
+  }
+};
