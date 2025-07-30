3eb6c74b89c79733a7f10a50c9977e003bbc468c
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-30 18:44:55 +0600

    Update mylove.js

--- src/commands/love/mylove.js
+++ src/commands/love/mylove.js
@@ -1,1 +1,19 @@
+module.exports = {
+  config: {
+    name: "mylove",
+    version: "1.0",
+    author: "newton",
+    countDown: 1,
+    role: 0,
+    shortDescription: "your love",
+    longDescription: "Shows who loves you the most",
+    category: "love",
+    guide: "{pn}"
+  },
 
+  onStart: async function ({ event, usersData, message }) {
+    const users = await usersData.getAll();
+    const pick = users[Math.floor(Math.random() * users.length)];
+    message.send(`💖 তোমাকে সবচেয়ে বেশি ভালোবাসে: ${pick.name}`);
+  }
+};
