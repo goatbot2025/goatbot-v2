580c4c29c09a576cedb48453db5a6a36c86625b6
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-30 18:54:38 +0600

    Update spy.js

--- src/commands/information/spy.js
+++ src/commands/information/spy.js
@@ -1,1 +1,21 @@
+module.exports = {
+  config: {
+    name: "spy",
+    version: "1.0",
+    author: "newton",
+    countDown: 10,
+    role: 2,
+    shortDescription: "Spy a user",
+    longDescription: "Spy on user's last seen or activity",
+    category: "information",
+    guide: "{pn} <@mention>"
+  },
 
+  onStart: async function ({ event, message }) {
+    const mention = Object.keys(event.mentions)[0];
+    if (!mention) return message.send("🕵️ কাউকে ট্যাগ কর!");
+
+    message.send(`👁️ তুমি ${mention} এর ওপর নজর রাখছো...`);
+    // Place logic here if using external presence API
+  }
+};
