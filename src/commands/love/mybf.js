77e59058fa4997b32367ec8204b0005cca5cb911
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-30 18:44:27 +0600

    Update mybf.js

--- src/commands/love/mybf.js
+++ src/commands/love/mybf.js
@@ -1,1 +1,19 @@
+module.exports = {
+  config: {
+    name: "mybf",
+    version: "1.0",
+    author: "newton",
+    countDown: 1,
+    role: 0,
+    shortDescription: "your BF",
+    longDescription: "Shows your imaginary boyfriend",
+    category: "love",
+    guide: "{pn}"
+  },
 
+  onStart: async function ({ message }) {
+    const names = ["Arif", "Siam", "Joy", "Tanvir", "Akash", "Nirob"];
+    const pick = names[Math.floor(Math.random() * names.length)];
+    message.send(`💙 তোমার বয়ফ্রেন্ড: ${pick}`);
+  }
+};
