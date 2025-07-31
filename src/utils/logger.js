d118e6d531472509975138a3e5355dcf703566bf
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-31 14:50:14 +0600

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,1 +1,24 @@
+const chalk = require("chalk");
 
+function logInfo(...args) {
+  console.log(chalk.blue("[INFO]"), ...args);
+}
+
+function logSuccess(...args) {
+  console.log(chalk.green("[SUCCESS]"), ...args);
+}
+
+function logWarning(...args) {
+  console.log(chalk.yellow("[WARN]"), ...args);
+}
+
+function logError(...args) {
+  console.log(chalk.red("[ERROR]"), ...args);
+}
+
+module.exports = {
+  logInfo,
+  logSuccess,
+  logWarning,
+  logError
+};
