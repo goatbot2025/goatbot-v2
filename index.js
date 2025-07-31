2a7205870f5ef7b27f40c7c53c9e397806bdd48e
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-31 15:36:52 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const { eventsHandler } = require('./src/handlers/eventsHandler');
+const { commandsHandler } = require('./src/handlers/commandsHandler');
+const { loadEnvironment } = require('./src/handlers/loader');
 
+(async () => {
+  try {
+    await loadEnvironment();
+    eventsHandler();
+    commandsHandler();
+  } catch (err) {
+    console.error('🔥 Bot initialization error:', err);
+  }
+})();
