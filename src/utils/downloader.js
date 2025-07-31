c3f0f057b15b1f928511d84fb7df0aba48862d16
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-31 14:51:25 +0600

    Update downloader.js

--- src/utils/downloader.js
+++ src/utils/downloader.js
@@ -1,1 +1,21 @@
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
+async function downloadFile(url, outputPath) {
+  const writer = fs.createWriteStream(outputPath);
+  const response = await axios({
+    url,
+    method: "GET",
+    responseType: "stream"
+  });
+
+  response.data.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on("finish", () => resolve(outputPath));
+    writer.on("error", reject);
+  });
+}
+
+module.exports = { downloadFile };
