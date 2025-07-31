0d0f3e4c5424579d249c3a6e276248fe8cb52697
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-31 15:14:40 +0600

    Update loader.js

--- src/handlers/loader.js
+++ src/handlers/loader.js
@@ -1,1 +1,14 @@
+const { loadCommands } = require("./commandsHandler");
+const { loadEvents } = require("./eventsHandler");
+const logger = require("../utils/logger.js");
 
+function loadAllHandlers() {
+  logger.info("🔄 Loading all handlers...");
+  loadCommands();
+  loadEvents();
+  logger.info("✅ All handlers loaded successfully.");
+}
+
+module.exports = {
+  loadAllHandlers
+};
