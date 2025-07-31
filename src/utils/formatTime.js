1b1e88bf5d8885180e6ee666b5cf8f4b3e33c119
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-31 14:50:47 +0600

    Update formatTime.js

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,1 +1,8 @@
+function formatTime(seconds) {
+  const hrs = Math.floor(seconds / 3600);
+  const mins = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+  return `${hrs}h ${mins}m ${secs}s`;
+}
 
+module.exports = formatTime;
