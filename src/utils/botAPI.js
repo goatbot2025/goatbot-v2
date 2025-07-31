46b783ca3d34016e1824c30c909ad83a71bdeafb
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-31 13:19:40 +0600

    Update botAPI.js

--- src/utils/botAPI.js
+++ src/utils/botAPI.js
@@ -1,1 +1,14 @@
+const axios = require("axios");
 
+module.exports = {
+  getStreamFromURL
+};
+
+async function getStreamFromURL(url) {
+  const response = await axios({
+    method: "GET",
+    url,
+    responseType: "stream"
+  });
+  return response.data;
+}
