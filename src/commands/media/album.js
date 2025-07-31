f119ae1dfe309a97d05049a1dac268e3244d64fa
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-31 11:55:45 +0600

    Update album.js

--- src/commands/media/album.js
+++ src/commands/media/album.js
@@ -1,1 +1,3 @@
+module.exports = { config: { name: "album", version: "1.0", author: "newton", role: 0, shortDescription: "Send album image", longDescription: "Sends a random album-style image", category: "media", guide: "{pn}" },
 
+onStart: async function ({ message }) { const images = [ "https://files.catbox.moe/album1.jpg", "https://files.catbox.moe/album2.jpg" ]; const img = images[Math.floor(Math.random() * images.length)]; message.reply({ attachment: await global.utils.getStreamFromURL(img) }); } };
